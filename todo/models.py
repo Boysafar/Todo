bb8bede1241927ddf44db6f2c3589d7cc240058e
Boysafar <boysafarnurmatov@gmail.com>
2024-07-05 20:38:41 +0500

    initial commit

---
+++ todo/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Todo(models.Model):
+    STATUS_CHOICES = [
+        ("new", "Yangi"),
+        ("in_process", "Jarayonda"),
+        ("done", "Bajarildi"),
+        ("canceled", "Bekor qilindi")
+    ]
+
+    name = models.CharField("Ish nomi ", max_length=255)
+    description = models.TextField("Tafsiv" )
+    status = models.CharField("Holati", choices=STATUS_CHOICES, max_length=10)
+
